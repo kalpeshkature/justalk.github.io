5c30eec5b5bf7d423f626bf29358153f62fee75a
abishekarjun98 <62903189+abishekarjun98@users.noreply.github.com>
2024-06-30 13:54:20 +0530

    reverting back to constant peer id

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function startCall(type) {
     const videoContainer = document.getElementById('video-container');
     videoContainer.style.display = 'block';
 
-    peer = new Peer();
+    peer = new Peer('${topic}-${type}-call');
 
     peer.on('open', id => {
         console.log('Peer ID: ', id);
@@ -41,7 +41,7 @@ function joinCall(type) {
 
     peer.on('open', id => {
         console.log('Peer ID: ', id);
-        call = peer.call(id, localStream);
+        call = peer.call('${topic}-${type}-call', localStream);
         call.on('stream', remoteStream => {
             const remoteVideo = document.getElementById('remote-video');
             remoteVideo.srcObject = remoteStream;
